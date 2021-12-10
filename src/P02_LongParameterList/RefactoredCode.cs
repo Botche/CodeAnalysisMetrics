c5b6d12f1658d4259958009604d3b33c84d06f4a
Gabriel <47752724+Botche@users.noreply.github.com>
2021-12-10 22:37:37 +0200

    Second problem is about Long Parameter List

---
+++ src/P02_LongParameterList/RefactoredCode.cs
@@ -0,0 +1,47 @@
+﻿namespace P02_LongParameterList
+{
+	public class RefactoredCode
+	{
+		public class Rectangle
+		{
+			public Rectangle(Point pointA, Point pointB)
+			{
+				this.PointA = pointA;
+				this.PointB = pointB;
+			}
+
+			public Point PointA { get; }
+
+			public Point PointB { get; }
+		}
+
+		public class Triangle
+		{
+			public Triangle(Point pointA, Point pointB, Point pointC)
+			{
+				this.PointA = pointA;
+				this.PointB = pointB;
+				this.PointC = pointC;
+			}
+
+			public Point PointA { get; }
+
+			public Point PointB { get; }
+
+			public Point PointC { get; }
+		}
+
+		public class Point
+		{
+			public Point(double x, double y)
+			{
+				this.X = x;
+				this.Y = y;
+			}
+
+			public double X { get; }
+
+			public double Y { get; }
+		}
+	}
+}
