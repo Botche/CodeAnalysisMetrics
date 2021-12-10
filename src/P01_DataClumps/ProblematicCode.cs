b93ee2723aefc9646d56f3320852a0d943287c21
Gabriel <47752724+Botche@users.noreply.github.com>
2021-12-10 22:37:21 +0200

    First problem is about Data Clumps

---
+++ src/P01_DataClumps/ProblematicCode.cs
@@ -0,0 +1,52 @@
+﻿namespace P01_DataClumps
+{
+	public class ProblematicCode
+	{
+		public class Rectangle
+		{
+			public Rectangle(double x1, double y1, double x2, double y2)
+			{
+				this.X1 = x1;
+				this.Y1 = y1;
+
+				this.X2 = x2;
+				this.Y2 = y2;
+			}
+
+			public double X1 { get; }
+
+			public double Y1 { get; }
+
+			public double X2 { get; }
+
+			public double Y2 { get; }
+		}
+
+		public class Triangle
+		{
+			public Triangle(double x1, double y1, double x2, double y2, double x3, double y3)
+			{
+				this.X1 = x1;
+				this.Y1 = y1;
+
+				this.X2 = x2;
+				this.Y2 = y2;
+
+				this.X3 = x3;
+				this.Y3 = y3;
+			}
+
+			public double X1 { get; }
+
+			public double Y1 { get; }
+
+			public double X2 { get; }
+
+			public double Y2 { get; }
+
+			public double X3 { get; }
+
+			public double Y3 { get; }
+		}
+	}
+}
---
+++ src/P01_DataClumps/RefactoredCode.cs
@@ -0,0 +1,47 @@
+﻿namespace P01_DataClumps
+{
+	public class RefactoredCode
+	{
+		public class Rectangle
+		{
+			public Rectangle(double x1, double y1, double x2, double y2)
+			{
+				this.PointA = new Point(x1, y1);
+				this.PointB = new Point(x2, y2);
+			}
+
+			public Point PointA { get; }
+
+			public Point PointB { get; }
+		}
+
+		public class Triangle
+		{
+			public Triangle(double x1, double y1, double x2, double y2, double x3, double y3)
+			{
+				this.PointA = new Point(x1, y1);
+				this.PointB = new Point(x2, y2);
+				this.PointC = new Point(x3, y3);
+			}
+
+			public Point PointA { get; }
+
+			public Point PointB { get; }
+
+			public Point PointC { get; }
+		}
+
+		public class Point
+		{
+			public Point(double x, double y)
+			{
+				this.X = x;
+				this.Y = y;
+			}
+
+			public double X { get; }
+
+			public double Y { get; }
+		}
+	}
+}
